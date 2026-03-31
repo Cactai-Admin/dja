8ab87f28a5701e09564818c2036acc80d0cc861f
Admin <admin@cactai.io>
2026-03-30 21:41:51 -0700

    Updated app-build-manifest.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "24b391e350553f45"; }
+/******/ 		__webpack_require__.h = function() { return "53fadcaf054735cb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
