748c6f53da38d36bcf7fdeab2bc686cc61ef6bbf
Admin <admin@cactai.io>
2026-03-30 22:38:26 -0700

    Updated job-card.tsx

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8e997fb55833295e"; }
+/******/ 		__webpack_require__.h = function() { return "da382ec2affc54d9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
