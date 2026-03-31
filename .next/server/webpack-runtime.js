2c800559c5b966af5b2e9cc81fcae96541eb620d
Admin <admin@cactai.io>
2026-03-30 21:33:57 -0700

    Update Supabase config

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("c707b0d0cefc01de")
+/******/ 		__webpack_require__.h = () => ("578cd6abd5910f46")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "778ee60c2a56ea75"; }
+/******/ 		__webpack_require__.h = function() { return "24b391e350553f45"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
